c0ec8f1819be8878d478f0492178a61a14fb4ef6
songul <songulmesale@gmail.com>
2025-05-31 00:14:14 +0300

    after create a triangle

--- Main.cpp
+++ Main.cpp
@@ -1,14 +1,30 @@
 #include<iostream>
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
+#include <cmath>
+
+// Vertex Shader source code
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+//Fragment Shader source code
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(0.5f, 0.9f, 0.7f, 1.0f);\n"
+
+"}\n\0";
+
 
 int main()
 {
 	// Initialize GLFW
 	glfwInit();
-    ///changed
-
-	//jkjkjhjjjlkkjkhkddkjjjýoýj
+    
 	// Tell GLFW what version of OpenGL we are using 
 	// In this case we are using OpenGL 3.3
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -16,12 +32,18 @@ int main()
 	// Tell GLFW we are using the CORE profile
 	// So that means we only have the modern functions
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	//flrfökrmfkmfhjhg
+	
+	//To learn about our monitor size
+	//GLFWmonitor* monitor = glfwGetPrimaryMonitor();        // get the main monitor
+	//const GLFWvidmode* mode = glfwGetVideoMode(monitor);    // get the video mode of the monitor
+	//std::cout << "Width: " << mode->width << std::endl;     // >> 1920
+	//std::cout << "Height: " << mode->height << std::endl;   // >> 1080
+	//std::cout << "Refresh rate: " << mode->refreshRate << " Hz" << std::endl;// >>144
 
 
 
 	// Create a GLFWwindow object of 800 by 800 pixels, naming it "YoutubeOpenGL"
-	GLFWwindow* window = glfwCreateWindow(800, 800, "YoutubeOpenGL", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(800,800, "YoutubeOpenGL", NULL, NULL);
 	// Error check if the window fails to create
 	if (window == NULL)
 	{
@@ -40,10 +62,71 @@ int main()
 	// In this case the viewport goes from x = 0, y = 0, to x = 800, y = 800
 	glViewport(0, 0, 800, 800);
 
+	// Create a vertex shader object and get ýts ID 
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	// load the vertex shader source code into the vertex shader object
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	// Compile the vertex shader into machine code
+	glCompileShader(vertexShader);
+
+	// Create a fragment shader object and get its ID
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	// load the fragment shader source code into the fragment shader object 
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	// compile the fragment shader into machine code
+	glCompileShader(fragmentShader);
+
+	// create shader program object and get its ID 
+	// A shader program is a holder(container) of vertex and fragment shaders
+	// it is used to link the shaders together so that they can work in harmony
+	GLuint shaderProgram = glCreateProgram();
+	// Attach the vertex shader to the shader program	
+	glAttachShader(shaderProgram, vertexShader);
+	// Attach the fragment shader to the shader program
+	glAttachShader(shaderProgram, fragmentShader);
+	// Links the vertex and fragment shaders into one executable shader program.
+	glLinkProgram(shaderProgram);
+
+	// Delete the vertex and fragment shaders as they are no longer needed
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	// Vertices coordinates;
+	GLfloat vertices[] =
+	{
+		-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower left corner
+		0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower right corner
+		0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f // Upper corner
+	};
+
+	// Create reference containers for the Vartex Array Object and the Vertex Buffer Object
+	GLuint VAO, VBO;
+	//// Generate the VAO and VBO with only 1 object each get its ID
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+
+	// Make the VAO the current Vertex Array Object by binding it
+	glBindVertexArray(VAO);
+
+	// Bind VBO and specify it as a GL_ARRAY_BUFFER
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	// LOAD the vertices data into the VBO
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	// configure the vertex attributes so openGL knows how to interpret the vertex data
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+	// Enable the vertex attribute array at index 0 and now opengl knows to use ýt 
+	glEnableVertexAttribArray(0);
+
+	// Unbind the VBO and VAO so that we don't accidentally modify them if we bind another VBO or VAO
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
 
 
-	// Specify the color of the background
-	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+
+	// Specify the color of the background This color will be used to clear the back buffer
+	// The parameters are the red, green, blue and alpha values .The values are between 0.0 and 1.0 
+	// alpha is the transparency of the color
+	glClearColor(0.80f, 0.50f, 0.70f, 1.00f);
 	// Clean the back buffer and assign the new color to it
 	glClear(GL_COLOR_BUFFER_BIT);
 	// Swap the back buffer with the front buffer
@@ -54,11 +137,26 @@ int main()
 	// Main while loop
 	while (!glfwWindowShouldClose(window))
 	{
+		// Specify the color of the background This color will be used to clear the back buffer
+		glClearColor(0.80f, 0.50f, 0.70f, 1.00f);
+		// Clean the back buffer and assign the new color to it
+		glClear(GL_COLOR_BUFFER_BIT);
+		// tell opengl which shader program we want to use
+		glUseProgram(shaderProgram);
+		// Bind the VAO so that opengl knows which vertex array object to use
+		glBindVertexArray(VAO);
+		// Draw the triangle using the vertex array object
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		// Swap the back buffer with the front buffer
+		glfwSwapBuffers(window);
 		// Take care of all GLFW events
 		glfwPollEvents();
 	}
 
-
+	// Delete object we created
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteProgram(shaderProgram);
 
 	// Delete window before ending the program
 	glfwDestroyWindow(window);
--- notesFL.md
+++ notesFL.md
@@ -3,10 +3,48 @@
 - GLFW (Graphics Library Framework) is a lightweight, open-source library designed for managing windows, OpenGL contexts, and handling input like keyboard, mouse, and joystick events. It is commonly used in OpenGL, OpenGL ES, and Vulkan development, providing a simple API for creating cross-platform applications.
 - GLAD (GLAD Loader) is a lightweight OpenGL function loader that helps manage OpenGL extensions and function pointers. It is commonly used in graphics programming to simplify the process of accessing OpenGL functions across different platforms.
 - CMAKE A build system generator is a tool that creates configuration files for a build system, which is responsible for compiling and linking source code into executable programs.
-## glwf functions explanation
+### glwf functions explanation
 - glfwInit: Initializes the GLFW library. This function must be called before any other GLFW functions.
 - glfwTerminate: Cleans up and closes the GLFW library. This function should be called when you are done using GLFW.
 - glfwWindowHint: Sets window hints for the GLFW window. These hints control various aspects of the window's creation, such as its size, context version, and whether it should be resizable.  
+`glfwWindowHint (int hint,int value )`  
+-> to see possible hint parameters [windowhints](https://www.glfw.org/docs/latest/window_guide.html#window_hints)
+- glfwGetPrimaryMonitor: Returns a handle to the primary monitor. This function is used to get the default monitor for the system.
+- glfwGetVideoMode: Returns a pointer to the current video mode of a monitor. This function is used to get information about the display mode of a monitor.
+
+#### GLFW typedefs explanation 
+- GLFWwindow: A handle to a window created by GLFW. This type is used to represent the window in the GLFW API.
+- GLFWmonitor: A handle to a monitor. This type is used to represent a monitor in the GLFW API.   
+- GLFWvidmode: A structure that contains information about a video mode, such as width, height, and refresh rate. This structure is used to describe the display mode of a monitor.  
+### GLFW functions explanation (cont.)
+- glfwMakeContextCurrent: Sets the current OpenGL context for the specified window. This function is used to make the OpenGL context of a window current, allowing OpenGL commands to be executed on that context.
+- glfwWindowShouldClose(window): Check if user wants to close the window
+- glfwPollEvents: Processes events in the event queue. This function is used to handle input events, such as keyboard and mouse events, and update the window state.  
 
 
-### h3.3jjhj
+### creating a triangle  
+#### :brain: SHADER STAGE FUNCTIONS
+- ` GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER); `
+  - Creates a new shader object.
+  - The `GL_VERTEX_SHADER` tells OpenGL this will be a vertex shader.  
+  - Returns an ID (unsigned int) to refer to this shader.    
+- `glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);`    
+  - Attaches the source code (GLSL) to the shader object.   
+  - Parameters:
+  `vertexShader`: shader ID  
+  `1`: number of source strings  
+  `&vertexShaderSource`: pointer to the source code  
+  `NULL`: optional array of string lengths (NULL = auto)    
+	
+Example
+- So, the full process would be:
+
+You load your raw materials (vertex data) into the storage bins (VBOs).  
+You design your processing machine (write and compile your Vertex Shader and Fragment Shader).
+You create the complete assembly line (link the shader program).
+You create the instruction manual (VAO) that tells the assembly line exactly how to take materials from the bins (VBOs) and feed them into the machines (shaders).
+When you want to produce something (draw your object):
+You tell the factory, "Use this assembly line" (glUseProgram).
+You tell it, "And here are the instructions for how the material is laid out" (glBindVertexArray).
+Then, you say, "Start production!" (glDrawArrays).
+
