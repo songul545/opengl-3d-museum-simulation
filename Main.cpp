fc597ec18ae97a785bb369b972bcac68986cfa39
songul <songulmesale@gmail.com>
2025-06-02 05:08:14 +0300

    goıng 3d

--- Main.cpp
+++ Main.cpp
@@ -7,21 +7,33 @@
 #include"VAO.h"
 #include"EBO.h"
 #include<stb/stb_image.h>
+#include<glm/glm.hpp>
+#include<glm/gtc/matrix_transform.hpp>
+#include<glm/gtc/type_ptr.hpp>
+
+const unsigned int width = 800;
+const unsigned int height = 800;
+
 
 
 // Vertices coordinates;
 GLfloat vertices[] =
 {
 	// Positions            // Colors         // Texture Coords
-	-0.5f, -0.5f, 0.0f,   0.41f, 0.8f, 0.9f,   0.0f, 0.0f,// lower left 
-	-0.5f, 0.5f, 0.0f,   0.7f, 0.03f, 0.12f,    0.0f, 1.0f,//  upper left
-	0.5f, 0.5f, 0.0f,   0.11f, 0.96f, 0.5f,      1.0f, 1.0f,// Upper right
-	0.5f, -0.5f, 0.0f,   0.91f, 0.35f, 0.65f,   1.0f, 0.0f// lower right
+	-0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,	   0.0f, 0.0f,
+	-0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	5.0f, 0.0f,
+	 0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	0.0f, 0.0f,
+	 0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,	5.0f, 0.0f,
+	 0.0f, 0.8f,  0.0f,     0.92f, 0.86f, 0.76f,	2.5f, 5.0f
 };
 
 GLuint indices[] = {
-	0, 1, 3, // First Triangle
-	1, 2, 3  // Second Triangle
+	0, 1, 2,
+	0, 2, 3,
+	0, 1, 4,
+	1, 2, 4,
+	2, 3, 4,
+	3, 0, 4
 };
 
 int main()
@@ -47,7 +59,7 @@ int main()
 
 
 	// Create a GLFWwindow object of 800 by 800 pixels, naming it "YoutubeOpenGL"
-	GLFWwindow* window = glfwCreateWindow(800,800, "YoutubeOpenGL", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(width,height, "YoutubeOpenGL", NULL, NULL);
 	// Error check if the window fails to create
 	if (window == NULL)
 	{
@@ -64,7 +76,7 @@ int main()
 	gladLoadGL();
 	// Specify the viewport of OpenGL in the Window
 	// In this case the viewport goes from x = 0, y = 0, to x = 800, y = 800
-	glViewport(0, 0, 800, 800);
+	glViewport(0, 0, width, height);
 
 	// Generates Shader object using shaders defualt.vert and default.frag
 	Shader shaderProgram("default.vert", "default.frag");
@@ -80,6 +92,7 @@ int main()
 	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 8* sizeof(GLfloat), (void*)0);
 	// for the second attribute (color)
 	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 8 * sizeof(GLfloat), (void*)(3 *sizeof(GLfloat)));
+	// for the third attribute (texture coordinates)
 	VAO1.LinkAttrib(VBO1, 2, 2, GL_FLOAT, 8 * sizeof(GLfloat), (void*)(6 *sizeof(GLfloat)));
 
 
@@ -92,8 +105,15 @@ int main()
 	GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
 
 	// Load the texture from the file and create a texture object
-	Texture trflag("trflag.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGB, GL_UNSIGNED_BYTE);
+	Texture trflag("brick.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
 	trflag.texUnit(shaderProgram, "tex0", 0); // Assign the texture unit to the shader uniform
+
+	// Variables that help the rotation of the pyramid
+    float rotation = 0.0f;
+	double prevTime = glfwGetTime();
+
+	// Enables the Depth Buffer so ýt doesnt look like the pyramid is going through itself
+	glEnable(GL_DEPTH_TEST);
 
 
 
@@ -103,16 +123,45 @@ int main()
 		// Specify the color of the background This color will be used to clear the back buffer
 		glClearColor(0.80f, 0.50f, 0.70f, 1.00f);
 		// Clean the back buffer and assign the new color to it
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		// tell opengl which shader program we want to use
 		shaderProgram.Activate();
+		// Simple timer
+		double crntTime = glfwGetTime();
+		if (crntTime - prevTime >= (1.0 / 60.0)) // If 1/60 seconds have passed, we rotate the pyramid
+		{
+			rotation += 0.5f; // Rotate the pyramid by 0.5 degrees 
+			// both rotatýon and time diffrence affect the speed
+			prevTime = crntTime;
+		}
+
+		glm::mat4 model = glm::mat4(1.0f); // Create a model matrix to transform the object like scaling, rotation, translation etc.
+		glm::mat4 view = glm::mat4(1.0f); // Create a view matrix to position the camera
+		glm::mat4 proj = glm::mat4(1.0f);  //defining how a 3d object is projected onto a 2d screen
+
+		// Rotate the model around the Y - axis
+		model = glm::rotate(model, glm::radians(rotation), glm::vec3(0.0f, 1.0f, 0.0f)); 
+		//view = glm::rotate(view, glm::radians(-10.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+		view = glm::translate(view, glm::vec3(0.0f, -0.5f, -2.0f));
+		proj = glm::perspective(glm::radians(45.0f),(float)(width/height), 0.1f, 100.0f); 
+		// Create a perspective projection matrix with a 45 degree field of view, aspect ratio of width/height, near plane at 0.1 and far plane at 100.0
+
+
+		// Outputs the matrices into the Vertex Shader
+		int modelLoc = glGetUniformLocation(shaderProgram.ID, "model");
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		int viewLoc = glGetUniformLocation(shaderProgram.ID, "view");
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+		int projLoc = glGetUniformLocation(shaderProgram.ID, "proj");
+		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(proj));
+
 		// assinging a value to unýform and always before activating the shaderprogram
-		glUniform1f(uniID, 0.6f);
+		glUniform1f(uniID, 0.5f);
 		// Bind the VAO so that opengl knows which vertex array object to use
 		trflag.Bind(); // Bind the texture so ýt appears ýn render 
 		VAO1.Bind();
 		// Draw the triangle using the indices
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glDrawElements(GL_TRIANGLES, sizeof(indices)/sizeof(int), GL_UNSIGNED_INT, 0);
 		// Swap the back buffer with the front buffer
 		glfwSwapBuffers(window);
 		// Take care of all GLFW events
--- default.vert
+++ default.vert
@@ -6,10 +6,13 @@ out vec3 color; // Output color to fragment shader
 out vec2 TexCoord; // Output texture coordinate to fragment shader
 
 uniform float scale;		// declared uniform variable for scaling
+uniform mat4 model; // Model matrix for transformations
+uniform mat4 view; // View matrix for camera transformations
+uniform mat4 proj; // Projection matrix for perspective 
 
 void main()
 {                      // apply the scale to the vertex position
-   gl_Position = vec4(aPos.x + aPos.x * scale , aPos.y + aPos.y * scale, aPos.z + aPos.z * scale, 1.0); 
+     gl_Position = proj * view * model * vec4(aPos, 1.0);
    color = aColor;
    TexCoord = aTex; // Pass texture coordinates to fragment shader
 
